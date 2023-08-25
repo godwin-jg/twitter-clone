a0067039c2b97b6926aff4946cadb9e021aac690
Godwin <jgnsecrets@gmail.com>
2023-08-25 14:26:28 +0530

    25-08-23_commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,26 @@ import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
-
+import Head from "next/head";
+import { SideNav } from "~/components/SideNav";
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Head>
+        <title>Twitter clone</title>
+        <meta name="description" content="by jg"/>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="container mx-auto flex items-start sm:pr-4">
+        <SideNav/>
+        <div className="min-h-screen flex-grow border-x">
+            <Component {...pageProps} />
+        </div>
+
+      </div>
     </SessionProvider>
   );
 };
