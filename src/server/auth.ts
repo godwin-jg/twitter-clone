e60172c9a82f544a1db7ded770f1063e2fdfe3bf
Godwin <jgnsecrets@gmail.com>
2023-08-28 23:33:52 +0530

    google auth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -6,6 +6,14 @@ import {
   type DefaultSession,
 } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
+import GoogleProvider from "next-auth/providers/google";
+// ...
+// providers: [
+//   GoogleProvider({
+//     clientId: process.env.GOOGLE_CLIENT_ID,
+//     clientSecret: process.env.GOOGLE_CLIENT_SECRET
+//   })
+// ]
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
 
@@ -50,6 +58,17 @@ export const authOptions: NextAuthOptions = {
     DiscordProvider({
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
+    }),
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
     }),
     /**
      * ...add more providers here.
